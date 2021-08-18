980e182a5232c0ce6c4bd45ca79c3397d18c5ccc
Blake Rain <blake.rain@blakerain.com>
2021-08-18 22:44:40 +0100

    Update insert-blog-posts.py

--- tools/insert-blog-posts/insert-blog-posts.py
+++ tools/insert-blog-posts/insert-blog-posts.py
@@ -1,4 +1,6 @@
+import json
 import requests
+from bs4 import BeautifulSoup
 
 MARK_START = "<!-- BLOGPOSTS_START -->"
 MARK_END = "<!-- BLOGPOSTS_END -->"
@@ -9,8 +11,12 @@ def postUrl(post):
 
 
 if __name__ == "__main__":
-    res = requests.get("https://blakerain.com/blog/routeInfo.json").json()
-    print(f"Found {len(res['data']['posts'])} blog post(s)")
+    html = requests.get("https://blakerain.com")
+    soup = BeautifulSoup(html.text, "html.parser")
+    src = soup.find(id="__NEXT_DATA__").string
+    obj = json.loads(src)
+    posts = obj["props"]["pageProps"]["posts"]
+    print(f"Found {len(posts)} blog post(s)")
     posts_markdown = '\n'.join([postUrl(post)
                                for post in res["data"]["posts"]])
     with open("README.md", "rt") as fp:
